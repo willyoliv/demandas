56f009a742343e152b1a9a7a65901f2c85a35f6e
willyoliv <willyoliversilva@gmail.com>
2022-05-21 18:02:42 -0300

    22 - Criando o Apollo Link, o Logger

--- packages/web/src/plugins/apollo/link.js
+++ packages/web/src/plugins/apollo/link.js
@@ -1,9 +1,21 @@
-import { ApolloLink } from 'apollo-link';
+import { ApolloLink, Observable } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { onError } from 'apollo-link-error';
 
+const loggerLink = new ApolloLink((operation, forward) => new Observable(observer => {
+  forward(operation).subscribe({
+    next: result => {
+      console.log('Log', result);
+      observer.next(result);
+    },
+    error: observer.complete.bind(observer),
+    complete: observer.complete.bind(observer),
+  });
+}));
+
 const link = ApolloLink.from([
+  loggerLink,
   onError((error) => {
     console.error('GraphQLError', error);
   }),
