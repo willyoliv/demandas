8201a24db59c75689c0dad66570187d292b82972
willyoliv <willyoliversilva@gmail.com>
2022-05-21 18:23:43 -0300

    23 - Apollo Client e Apollo DevTools

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import { ApolloProvider } from 'react-apollo';
+
+import client from './plugins/apollo/client';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
